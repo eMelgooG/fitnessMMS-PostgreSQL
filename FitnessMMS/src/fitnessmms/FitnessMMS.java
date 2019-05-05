3449f32db5419db696642ee3a0aa39e6d188cbe2
eMelgooG <manu_bc2004@hotmail.com>
2019-05-05 23:38:45 +0100

    Update FitnessMMS.java

--- FitnessMMS/src/fitnessmms/FitnessMMS.java
+++ FitnessMMS/src/fitnessmms/FitnessMMS.java
@@ -9,19 +9,19 @@ public class FitnessMMS {
       FileHandler fh = new FileHandler();
       ManagementSystem ms = new ManagementSystem();
       int choice;
-      m = fh.readFile();
-      choice = ms.getChoice();
-      String member;
+      m = fh.readFile();              // we read the csv file and store it into a LinkedList
+      String member;                  // store the a new added member and then append it to the csv file
+        choice = ms.getChoice();      // we start the program
       
-      while (choice!=-1) {
+      while (choice!=-1) {                              // -1 to exit the program
           switch (choice) {
-              case 1: member = ms.addMember(m);
+              case 1: member = ms.addMember(m);          //add a member
                       fh.appendFile(member);
                       break;
-              case 2: ms.removeMember(m);
+              case 2: ms.removeMember(m);                //remove a member
                       fh.removeFile(m);
                       break;
-              case 3: ms.displayMemInfo(m);
+              case 3: ms.displayMemInfo(m);              //display member info
                       break;
                        
               default: System.out.println("Invalid option!\n");
